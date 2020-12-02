8b277a09aef52a8aead0e278f292816bef33e988
Bruno Domingues <bruno_signorelli@outlook.com>
2020-12-02 08:35:50 -0300

    Finished views

--- tasks/serializer.py
+++ tasks/serializer.py
@@ -2,7 +2,8 @@ from rest_framework.serializers import ModelSerializer
 
 from .models import Task
 
-class TaskSerializer(ModelSerializer):
+
+class Serializer(ModelSerializer):
     class Meta:
         model = Task
-        fields = ('title', 'pub_date', 'description')
+        fields = ("title", "pub_date", "description")
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from tasks.models import Task
+from tasks.serializer import Serializer
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+    return HttpResponse("Hello! You're at the postgres index.")
+
+
+@api_view(["GET"])
+def get_all_tasks(request):
+    data = Serializer(Task.objects.all(), many=True)
+    return JsonResponse(data.data, safe=False)
+
+
+@api_view(["POST"])
+def create_task(request):
+    data = JSONParser().parse(request)
+    serializer = Serializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=200)
+    return JsonResponse(serializer.errors, status=400)
+
+
+@api_view(["DELETE"])
+def delete_all_tasks(request):
+    Task.objects.all().delete()
+    return HttpResponse("Tasks deleted successfully!")
