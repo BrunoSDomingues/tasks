c481102dcb755a4bc3f275a0aec6b37840e4460f
Bruno Signorelli Domingues <brunosd1@al.insper.edu.br>
2020-12-03 07:24:00 -0300

    Removed end backslash

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create_task/", views.create_task, name="create task"),
-    path("get_tasks/", views.get_all_tasks, name="get all tasks"),
-    path("delete_tasks/", views.delete_all_tasks, name="delete all tasks"),
+    path("create_task", views.create_task, name="create task"),
+    path("get_tasks", views.get_all_tasks, name="get all tasks"),
+    path("delete_tasks", views.delete_all_tasks, name="delete all tasks"),
 ]
